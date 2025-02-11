de8e5f9d7d9657b796ffa2878f301d2515413213
MuhammadrizoShuhratov <shuhratovmuhammadrizo24@gmail.com>
2025-02-11 23:39:11 +0500

    uy work add

---
+++ app.js
@@ -0,0 +1,40 @@
+let nom = +prompt("son kriting");
+
+if ( nom % 3 === 0 ) {
+    alert("Bu son 3 ga bo'linadi");
+} else {
+    alert("Bu son 3 ga bo'linmaydi");
+}
+
+
+
+let son = +prompt("son kriting");
+if ( son % 5 === 0 ) {
+    alert("Bu son 5 ga bo'linadi");
+} else {
+    alert("Bu son 5  ga bo'linmaydi");
+}
+
+
+
+
+let yozuv = +prompt("son kriting");
+
+if ( yozuv % 3 === 0 && son % 5 === 0 ) {
+    alert("Bu son 3 ga ham 5 ga ham bo'linadi");
+} else {
+    alert("Bu son 3 ga ham 5 ga ham bo'linmaydi");
+}
+
+
+
+
+
+
+
+// if ( son % 5 === 0 ) {
+//     alert("Bu son 5 ga bo'linadi")
+// } else {
+//     alert("Bu son 5  ga bo'linmaydi")
+// }
+
