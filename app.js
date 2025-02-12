70648d91e056cedaaa34e225e2f373d338dcfcc9
MuhammadrizoShuhratov <shuhratovmuhammadrizo24@gmail.com>
2025-02-12 14:11:20 +0500

    code add

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-let nom = +prompt("son kriting");
+// let nom = +prompt("son kriting");
 
-if ( nom % 3 === 0 ) {
-    alert("Bu son 3 ga bo'linadi");
+// if ( nom % 3 === 0 ) {
+//     alert("Bu son 3 ga bo'linadi");
+// } else {
+//     alert("Bu son 3 ga bo'linmaydi");
+// }
+
+
+
+// let son = +prompt("son kriting");
+// if ( son % 5 === 0 ) {
+//     alert("Bu son 5 ga bo'linadi");
+// } else {
+//     alert("Bu son 5  ga bo'linmaydi");
+// }
+
+
+
+
+// let yozuv = +prompt("son kriting");
+
+// if ( yozuv % 3 === 0 && son % 5 === 0 ) {
+//     alert("Bu son 3 ga ham 5 ga ham bo'linadi");
+// } else {
+//     alert("Bu son 3 ga ham 5 ga ham bo'linmaydi");
+// }
+
+
+
+
+
+let page = +prompt("parolni kiriting");
+let zur = String("rizo2010");
+if (page || zur) {
+    alert("to'gri")
 } else {
-    alert("Bu son 3 ga bo'linmaydi");
+    alert("notug'ri")
 }
-
-
-
-let son = +prompt("son kriting");
-if ( son % 5 === 0 ) {
-    alert("Bu son 5 ga bo'linadi");
-} else {
-    alert("Bu son 5  ga bo'linmaydi");
-}
-
-
-
-
-let yozuv = +prompt("son kriting");
-
-if ( yozuv % 3 === 0 && son % 5 === 0 ) {
-    alert("Bu son 3 ga ham 5 ga ham bo'linadi");
-} else {
-    alert("Bu son 3 ga ham 5 ga ham bo'linmaydi");
-}
-
-
-
-
-
 
 
 // if ( son % 5 === 0 ) {
